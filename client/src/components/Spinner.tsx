213f073804e6307804d7d065263e234ac98a2383
Anton Konovalov <antoniuskonovalov@gmail.com>
2023-02-08 15:05:23 +0200

    adding ; spinner

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,5 +1,5 @@
-import Lottie from 'lottie-react'
-import spinner from '../assets/svg/loader.json'
+import Lottie from 'lottie-react';
+import spinner from '../assets/svg/loader.json';
 
 
 const Spinner = () => {
